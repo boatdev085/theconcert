4e0c7a43e64d6e51c65f88e6aaa251925bdfa4c4
Itthi Ploysekum <itthi.p@wirtual.co>
2023-07-26 02:00:22 +0700

    fix select ticket

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// Import puppeteer
+const axios_1 = __importDefault(require("axios"));
 const puppeteer_1 = __importDefault(require("puppeteer"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
@@ -22,6 +24,7 @@ function delay(time) {
 }
 (() => __awaiter(void 0, void 0, void 0, function* () {
     // Launch the browser
+    var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k, _l, _m;
     //   const response = await axios.post(
     //     'https://www.theconcert.com/rest/users/login',
     //     {
@@ -48,33 +51,49 @@ function delay(time) {
     });
     //   // Create a page
     const page = yield browser.newPage();
+    // const cookies = fs.readFileSync('cookies.json', 'utf8');
+    // if (cookies) {
+    //   const deserializedCookies = JSON.parse(cookies);
+    //   await page.setCookie(...deserializedCookies);
+    // }
+    // console.log('cookies: ', cookies);
     //   // Go to your site
-    yield page.goto(`https://www.theconcert.com/concert/${process.env.ID_CONCERT}`);
+    yield page.goto(`https://www.theconcert.com/`);
     // Query for an element handle.
-    const btnReserve = yield page.waitForSelector('.btn-reserve');
-    // Do something with element...
-    console.log('btnReserve: ', btnReserve);
-    if (btnReserve) {
-        yield btnReserve.click(); // Just an example.
-        yield btnReserve.dispose();
-        // Dispose of handle
+    // const localStorageFile = fs.readFileSync('localstorage.json', 'utf8');
+    // const deserializedStorage = JSON.parse(localStorageFile);
+    // console.log('deserializedStorage: ', deserializedStorage);
+    // await page.evaluate((deserializedStorage) => {
+    //   for (const key in deserializedStorage) {
+    //     localStorage.setItem(key, deserializedStorage[key]);
+    //   }
+    // }, deserializedStorage);
+    yield delay(1500);
+    yield page.waitForSelector('#popup-modal');
+    const btnBeforeLogin = yield page.waitForSelector('#shortcut_bar > div > div > div.short-profile.cursor-pointer.d-none.d-md-block');
+    if (btnBeforeLogin) {
+        yield page.$eval('#shortcut_bar > div > div > div.short-profile.cursor-pointer.d-none.d-md-block', (elm) => elm.click());
     }
-    yield delay(500);
-    const menuLogin = yield page.waitForSelector('.login-box > ul > li:nth-child(2)');
-    console.log('menuLogin: ', menuLogin);
-    if (menuLogin) {
-        yield menuLogin.click();
-    }
-    yield page.waitForSelector('#email-login');
-    yield page.focus('#email-login');
-    yield page.keyboard.type(process.env.USERNAME || '', { delay: 10 });
-    yield page.waitForSelector('#password-field-email');
-    yield page.focus('#password-field-email');
-    yield page.keyboard.type(process.env.PASSWORD || '', { delay: 10 });
-    const btnLogin = yield page.waitForSelector('#login_email_btn');
     yield delay(1000);
-    //   await page.setRequestInterception(true);
-    yield (btnLogin === null || btnLogin === void 0 ? void 0 : btnLogin.click());
+    yield page.waitForSelector('#_login');
+    yield page.focus('#_login');
+    yield page.keyboard.type(process.env.USERNAME || '', { delay: 10 });
+    yield page.waitForSelector('#password-field');
+    yield page.focus('#password-field');
+    yield page.keyboard.type(process.env.PASSWORD || '', { delay: 10 });
+    yield page.waitForSelector('#login_btn');
+    yield page.$eval('#login_btn', (elm) => elm.click());
+    // console.log('process.env: ', process.env);
+    // await page.waitForSelector('#email-login');
+    // await page.focus('#email-login');
+    // await page.keyboard.type(process.env.USERNAME || '', { delay: 10 });
+    // await page.waitForSelector('#password-field-email');
+    // await page.focus('#password-field-email');
+    // await page.keyboard.type(process.env.PASSWORD || '', { delay: 10 });
+    // const btnLogin = await page.waitForSelector('#login_email_btn');
+    // await delay(1000);
+    // //   await page.setRequestInterception(true);
+    // await btnLogin?.click();
     const getAccessToken = yield new Promise((resolve) => {
         page.on('response', (response) => __awaiter(void 0, void 0, void 0, function* () {
             var _a;
@@ -85,32 +104,85 @@ function delay(time) {
             }
         }));
     });
-    //   console.log('getAccessToken', getAccessToken);
+    // if (getAccessToken) {
+    //   const cookies = await page.cookies();
+    //   const cookieJson = JSON.stringify(cookies);
+    //   fs.writeFileSync('cookies.json', cookieJson);
+    //   const localStorage = await page.evaluate(() =>
+    //     JSON.stringify(window.localStorage)
+    //   );
+    //   fs.writeFileSync('localstorage.json', localStorage);
+    // }
+    // // fs.writeFileSync('cookies.json', cookieJson);
+    // //   console.log('getAccessToken', getAccessToken);
+    // if (!getAccessToken) {
+    //   console.log('login fail');
+    //   return;
+    // }
     const page2 = yield browser.newPage();
-    //   // Go to your site
+    // //   // Go to your site
     yield page2.goto(`https://www.theconcert.com/concert/${process.env.ID_CONCERT}/stage?queue=${getAccessToken}`);
-    let getZoneBuy = yield page2.waitForSelector('#zone-box > ul > li:nth-child(5)');
-    console.log('getZoneBuy: ', getZoneBuy);
-    if (getZoneBuy) {
-        yield getZoneBuy.click();
+    const getList = yield axios_1.default.get(`https://api.theconcert.com/v3/products/${process.env.ID_CONCERT}/variants?limit=10&page=1`);
+    if (((_a = getList === null || getList === void 0 ? void 0 : getList.data) === null || _a === void 0 ? void 0 : _a.data) && ((_d = (_c = (_b = getList === null || getList === void 0 ? void 0 : getList.data) === null || _b === void 0 ? void 0 : _b.data) === null || _c === void 0 ? void 0 : _c.record) === null || _d === void 0 ? void 0 : _d.length) > 0) {
+        for (let i = 0; i < ((_g = (_f = (_e = getList === null || getList === void 0 ? void 0 : getList.data) === null || _e === void 0 ? void 0 : _e.data) === null || _f === void 0 ? void 0 : _f.record) === null || _g === void 0 ? void 0 : _g.length); i++) {
+            if (!((_m = (_l = (_k = (_j = (_h = getList === null || getList === void 0 ? void 0 : getList.data) === null || _h === void 0 ? void 0 : _h.data) === null || _j === void 0 ? void 0 : _j.record[i]) === null || _k === void 0 ? void 0 : _k.display_text) === null || _l === void 0 ? void 0 : _l.toLowerCase()) === null || _m === void 0 ? void 0 : _m.includes('sold'))) {
+                let getZoneBuy = yield page2.waitForSelector(`#zone-box > ul > li:nth-child(${i + 1})`);
+                console.log('zone', i + 1);
+                if (getZoneBuy) {
+                    yield getZoneBuy.click();
+                    break;
+                }
+            }
+            continue;
+        }
     }
-    yield delay(1000);
-    let getChair = yield page2.waitForSelector('#wrap > svg > image:nth-child(1)');
-    console.log('getChair: ', getChair);
-    if (getChair) {
-        yield getChair.click();
+    yield delay(2000);
+    const position = yield page2.$$eval('#wrap > svg > image', (elms) => {
+        let positionPass = [];
+        elms === null || elms === void 0 ? void 0 : elms.forEach((e) => {
+            var _a;
+            if ((_a = e.getAttribute('href')) === null || _a === void 0 ? void 0 : _a.includes('seat-blue.png')) {
+                const getPosition = e.getBoundingClientRect();
+                positionPass.push({ x: getPosition.x, y: getPosition.y });
+            }
+        });
+        return positionPass;
+    });
+    for (let p = 0; p < (position === null || position === void 0 ? void 0 : position.length); p++) {
+        yield page2.mouse.click(position[p].x, position[p].y, { button: 'left' });
     }
-    yield delay(100);
-    let getBtnSubmit = yield page2.waitForSelector('#select-variant > div.box-placeorder > div.button-f > button');
-    console.log('getBtnSubmit: ', getBtnSubmit);
-    //
-    if (getBtnSubmit) {
-        yield getBtnSubmit.click();
-    }
-    //   await page2.evaluate(() => {
-    //     const zoneList = document.querySelectorAll('#zone-box > ul > li');
-    //     console.log('zoneList: ', zoneList);
-    //   });
-    console.log('close');
-    //   await browser.close();
+    console.log('finish click');
+    yield delay(2000);
+    yield page2.$eval('#select-variant > div.box-placeorder > div.button-f > button', (elm) => elm.click());
+    // a.getBoundingClientRect()
+    // let getZoneBuy = await page2.waitForSelector(
+    //   '#zone-box > ul > li:nth-child(5)'
+    // );
+    // console.log('getZoneBuy: ', getZoneBuy);
+    // if (getZoneBuy) {
+    //   await getZoneBuy.click();
+    // }
+    // await delay(1000);
+    // let getChair = await page2.waitForSelector(
+    //   '#wrap > svg > image:nth-child(1)'
+    // );
+    // console.log('getChair: ', getChair);
+    // if (getChair) {
+    //   await getChair.click();
+    // }
+    // await delay(100);
+    // let getBtnSubmit = await page2.waitForSelector(
+    //   '#select-variant > div.box-placeorder > div.button-f > button'
+    // );
+    // console.log('getBtnSubmit: ', getBtnSubmit);
+    // //
+    // if (getBtnSubmit) {
+    //   await getBtnSubmit.click();
+    // }
+    // //   await page2.evaluate(() => {
+    // //     const zoneList = document.querySelectorAll('#zone-box > ul > li');
+    // //     console.log('zoneList: ', zoneList);
+    // //   });
+    // console.log('close');
+    // //   await browser.close();
 }))();
--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@
 import axios from 'axios';
 import puppeteer from 'puppeteer';
 import dotenv from 'dotenv';
+import fs from 'fs';
 dotenv.config();
 
 function delay(time: number) {
@@ -40,44 +41,63 @@ function delay(time: number) {
   });
   //   // Create a page
   const page = await browser.newPage();
+  // const cookies = fs.readFileSync('cookies.json', 'utf8');
+  // if (cookies) {
+  //   const deserializedCookies = JSON.parse(cookies);
+  //   await page.setCookie(...deserializedCookies);
+  // }
+
+  // console.log('cookies: ', cookies);
 
   //   // Go to your site
-  await page.goto(
-    `https://www.theconcert.com/concert/${process.env.ID_CONCERT}`
-  );
-
+  await page.goto(`https://www.theconcert.com/`);
   // Query for an element handle.
-  const btnReserve = await page.waitForSelector('.btn-reserve');
 
-  // Do something with element...
-  console.log('btnReserve: ', btnReserve);
-  if (btnReserve) {
-    await btnReserve.click(); // Just an example.
-    await btnReserve.dispose();
-    // Dispose of handle
-  }
-  await delay(500);
+  // const localStorageFile = fs.readFileSync('localstorage.json', 'utf8');
 
-  const menuLogin = await page.waitForSelector(
-    '.login-box > ul > li:nth-child(2)'
+  // const deserializedStorage = JSON.parse(localStorageFile);
+  // console.log('deserializedStorage: ', deserializedStorage);
+  // await page.evaluate((deserializedStorage) => {
+  //   for (const key in deserializedStorage) {
+  //     localStorage.setItem(key, deserializedStorage[key]);
+  //   }
+  // }, deserializedStorage);
+  await delay(1500);
+  await page.waitForSelector('#popup-modal');
+  const btnBeforeLogin = await page.waitForSelector(
+    '#shortcut_bar > div > div > div.short-profile.cursor-pointer.d-none.d-md-block'
   );
-  console.log('menuLogin: ', menuLogin);
-  if (menuLogin) {
-    await menuLogin.click();
+  if (btnBeforeLogin) {
+    await page.$eval(
+      '#shortcut_bar > div > div > div.short-profile.cursor-pointer.d-none.d-md-block',
+      (elm) => elm.click()
+    );
   }
+  await delay(1000);
 
-  await page.waitForSelector('#email-login');
-  await page.focus('#email-login');
+  await page.waitForSelector('#_login');
+  await page.focus('#_login');
   await page.keyboard.type(process.env.USERNAME || '', { delay: 10 });
 
-  await page.waitForSelector('#password-field-email');
-  await page.focus('#password-field-email');
+  await page.waitForSelector('#password-field');
+  await page.focus('#password-field');
   await page.keyboard.type(process.env.PASSWORD || '', { delay: 10 });
-  const btnLogin = await page.waitForSelector('#login_email_btn');
-  await delay(1000);
-  //   await page.setRequestInterception(true);
+  await page.waitForSelector('#login_btn');
+  await page.$eval('#login_btn', (elm: any) => elm.click());
 
-  await btnLogin?.click();
+  // console.log('process.env: ', process.env);
+  // await page.waitForSelector('#email-login');
+  // await page.focus('#email-login');
+  // await page.keyboard.type(process.env.USERNAME || '', { delay: 10 });
+
+  // await page.waitForSelector('#password-field-email');
+  // await page.focus('#password-field-email');
+  // await page.keyboard.type(process.env.PASSWORD || '', { delay: 10 });
+  // const btnLogin = await page.waitForSelector('#login_email_btn');
+  // await delay(1000);
+  // //   await page.setRequestInterception(true);
+
+  // await btnLogin?.click();
   const getAccessToken = await new Promise((resolve) => {
     page.on('response', async (response) => {
       const request = response.request();
@@ -87,45 +107,105 @@ function delay(time: number) {
       }
     });
   });
+  // if (getAccessToken) {
+  //   const cookies = await page.cookies();
+  //   const cookieJson = JSON.stringify(cookies);
+  //   fs.writeFileSync('cookies.json', cookieJson);
+  //   const localStorage = await page.evaluate(() =>
+  //     JSON.stringify(window.localStorage)
+  //   );
+  //   fs.writeFileSync('localstorage.json', localStorage);
+  // }
 
-  //   console.log('getAccessToken', getAccessToken);
+  // // fs.writeFileSync('cookies.json', cookieJson);
+  // //   console.log('getAccessToken', getAccessToken);
+  // if (!getAccessToken) {
+  //   console.log('login fail');
+  //   return;
+  // }
   const page2 = await browser.newPage();
 
-  //   // Go to your site
+  // //   // Go to your site
   await page2.goto(
     `https://www.theconcert.com/concert/${process.env.ID_CONCERT}/stage?queue=${getAccessToken}`
   );
 
-  let getZoneBuy = await page2.waitForSelector(
-    '#zone-box > ul > li:nth-child(5)'
+  const getList = await axios.get(
+    `https://api.theconcert.com/v3/products/${process.env.ID_CONCERT}/variants?limit=10&page=1`
   );
-  console.log('getZoneBuy: ', getZoneBuy);
-  if (getZoneBuy) {
-    await getZoneBuy.click();
+  if (getList?.data?.data && getList?.data?.data?.record?.length > 0) {
+    for (let i = 0; i < getList?.data?.data?.record?.length; i++) {
+      if (
+        !getList?.data?.data?.record[i]?.display_text
+          ?.toLowerCase()
+          ?.includes('sold')
+      ) {
+        let getZoneBuy = await page2.waitForSelector(
+          `#zone-box > ul > li:nth-child(${i + 1})`
+        );
+        console.log('zone', i + 1);
+        if (getZoneBuy) {
+          await getZoneBuy.click();
+          break;
+        }
+      }
+      continue;
+    }
   }
-  await delay(1000);
-  let getChair = await page2.waitForSelector(
-    '#wrap > svg > image:nth-child(1)'
-  );
-  console.log('getChair: ', getChair);
-  if (getChair) {
-    await getChair.click();
+  await delay(2000);
+  const position = await page2.$$eval('#wrap > svg > image', (elms) => {
+    let positionPass: any[] = [];
+    elms?.forEach((e) => {
+      if (e.getAttribute('href')?.includes('seat-blue.png')) {
+        const getPosition = e.getBoundingClientRect();
+        positionPass.push({ x: getPosition.x, y: getPosition.y });
+      }
+    });
+    return positionPass;
+  });
+  for (let p = 0; p < position?.length; p++) {
+    await page2.mouse.click(position[p].x, position[p].y, { button: 'left' });
   }
+  console.log('finish click');
 
-  await delay(100);
+  await delay(2000);
 
-  let getBtnSubmit = await page2.waitForSelector(
-    '#select-variant > div.box-placeorder > div.button-f > button'
+  await page2.$eval(
+    '#select-variant > div.box-placeorder > div.button-f > button',
+    (elm) => elm.click()
   );
-  console.log('getBtnSubmit: ', getBtnSubmit);
-  //
-  if (getBtnSubmit) {
-    await getBtnSubmit.click();
-  }
-  //   await page2.evaluate(() => {
-  //     const zoneList = document.querySelectorAll('#zone-box > ul > li');
-  //     console.log('zoneList: ', zoneList);
-  //   });
-  console.log('close');
-  //   await browser.close();
+
+  // a.getBoundingClientRect()
+  // let getZoneBuy = await page2.waitForSelector(
+  //   '#zone-box > ul > li:nth-child(5)'
+  // );
+  // console.log('getZoneBuy: ', getZoneBuy);
+  // if (getZoneBuy) {
+  //   await getZoneBuy.click();
+  // }
+  // await delay(1000);
+  // let getChair = await page2.waitForSelector(
+  //   '#wrap > svg > image:nth-child(1)'
+  // );
+  // console.log('getChair: ', getChair);
+  // if (getChair) {
+  //   await getChair.click();
+  // }
+
+  // await delay(100);
+
+  // let getBtnSubmit = await page2.waitForSelector(
+  //   '#select-variant > div.box-placeorder > div.button-f > button'
+  // );
+  // console.log('getBtnSubmit: ', getBtnSubmit);
+  // //
+  // if (getBtnSubmit) {
+  //   await getBtnSubmit.click();
+  // }
+  // //   await page2.evaluate(() => {
+  // //     const zoneList = document.querySelectorAll('#zone-box > ul > li');
+  // //     console.log('zoneList: ', zoneList);
+  // //   });
+  // console.log('close');
+  // //   await browser.close();
 })();
